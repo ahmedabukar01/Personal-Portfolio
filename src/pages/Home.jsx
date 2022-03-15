61d797cd8f7a1d9ca9697fe6bbb34a1bb1588588
ahmed <ahmedlearn01@gmail.com>
2022-03-15 06:53:21 -0700

    particles

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <div id='particles.js'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
