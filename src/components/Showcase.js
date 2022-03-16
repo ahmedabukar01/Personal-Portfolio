a0c95d8f835084bf34ca3185871d4ed2ceee8b18
ahmed <ahmedlearn01@gmail.com>
2022-03-16 08:15:43 -0700

    showcase

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import { AiFillGithub } from 'react-icons/ai'
 
 const Showcase = () => {
   return (
     <div className='showcase'>
-        
+        <h1>Ahmed Abukar Omar</h1>
+        <h3>FullStack Developer</h3>
+        <div className="links">
+            <a href="#">Download Cv</a>
+            <a href="#">Github <AiFillGithub /> </a>
+        </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 // import ParticleBackground from '../scripts/ParticleBackground';
 
 import Header from "../components/Header"
+import Showcase from "../components/Showcase"
 
 const Home = () => {
   return (
     <div className='home'>
       <Header />
+      <Showcase />
     </div>
   )
 }
