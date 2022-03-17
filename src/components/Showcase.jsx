810a202c0fa386ffb75b600a7d8508980db0115f
ahmed <ahmedlearn01@gmail.com>
2022-03-17 09:26:15 -0700

    links

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,4 +1,5 @@
 import { AiFillGithub } from 'react-icons/ai'
+import { MdDownloadForOffline } from 'react-icons/md'
 
 const Showcase = () => {
   return (
@@ -6,8 +7,8 @@ const Showcase = () => {
         <h1>Ahmed Abukar Omar</h1>
         <h3>FullStack Developer</h3>
         <div className="links">
-            <a href="#">Download Cv</a>
-            <a href="#">Github <AiFillGithub /> </a>
+            <a href="#" className='btn btn-cv'><MdDownloadForOffline className='icon'/> Download Cv</a>
+            <a href="#" className='btn btn-github'><AiFillGithub className='icon'/> Github</a>
         </div>
     </div>
   )
