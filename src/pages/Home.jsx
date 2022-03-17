6c53cc584678aa4cb6591115e83b29c436a22eab
ahmed <ahmedlearn01@gmail.com>
2022-03-17 10:33:05 -0700

    home styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Showcase from "../components/Showcase"
 const Home = () => {
   return (
     <div className='home'>
+      {/* <ParticleBackground /> */}
       <Header />
       <Showcase />
     </div>
