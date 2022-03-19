1140b8f8c559da4b06aae8d18cff6833d9a669f6
ahmed <ahmedlearn01@gmail.com>
2022-03-19 12:29:14 -0700

    resp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,24 @@
 import {FaBars} from 'react-icons/fa';
+import {AiOutlineClose} from 'react-icons/ai';
+import { useState } from 'react';
 
 const Header = () => {
+  const [show, setShow] = useState(true);
   return (
     <div className='header'>
         <div className="logo">
             <span>A<sup>2</sup>O</span>
         </div>
         <ul className="tabs">
-            <li><a href="#home">Home</a></li>
+          <li> <AiOutlineClose className='close'/> </li>
+            <li><a href="#home" >Home</a></li>
             <li><a href="#home">About</a></li>
             <li><a href="#home">Portfolio</a></li>
             <li><a href="#home">Blog</a></li>
             <li><a href="#home">Contact</a></li>
             <li><a href="#home">Footer</a></li>
         </ul>
-        <FaBars className="fa-bars"/>
+        {show && <FaBars className="fa-bars"/> }
     </div>
   )
 }
--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -7,7 +7,7 @@ const Showcase = () => {
         <h1>Ahmed Abukar Omar</h1>
         <h3>FullStack Developer</h3>
         <div className="links">
-            <a href="#" className='btn btn-cv'><MdDownloadForOffline className='icon'/> Download Cv</a>
+            <a href="#" className='btn btn-cv'><MdDownloadForOffline className='icon'/> Download CV</a>
             <a href="#" className='btn btn-github'><AiFillGithub className='icon'/> Github</a>
         </div>
     </div>
