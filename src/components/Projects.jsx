2dd99b7b7dd7e4e7c81ec872a233fd0209f60aab
ahmed <ahmedlearn01@gmail.com>
2022-03-23 09:06:16 -0700

    added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,6 +23,69 @@ const Projects = () => {
           <button>Demo</button>
         </div>
       </div>
+      
+      <div className="pro-wrap">
+        <img src={pro1} alt="pro" />
+        <h3>Somblog</h3>
+        <p>Complete Blog Website with Admin management & Moderators and User Login With Authentication</p>
+        <div className="tech">
+          <div className="tech-name">Tech</div>
+          <div className="technologies">
+            <div>React</div>
+            <div>Node js</div>
+            <div>Exress</div>
+            <div>MongoDB</div>
+            <div>JWT Authentication</div>
+            <div>CSS</div>
+          </div>
+        </div>
+        <div className="btn-pro">
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+
+      <div className="pro-wrap">
+        <img src={pro1} alt="pro" />
+        <h3>Somblog</h3>
+        <p>Complete Blog Website with Admin management & Moderators and User Login With Authentication</p>
+        <div className="tech">
+          <div className="tech-name">Tech</div>
+          <div className="technologies">
+            <div>React</div>
+            <div>Node js</div>
+            <div>Exress</div>
+            <div>MongoDB</div>
+            <div>JWT Authentication</div>
+            <div>CSS</div>
+          </div>
+        </div>
+        <div className="btn-pro">
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+
+      <div className="pro-wrap">
+        <img src={pro1} alt="pro" />
+        <h3>Somblog</h3>
+        <p>Complete Blog Website with Admin management & Moderators and User Login With Authentication</p>
+        <div className="tech">
+          <div className="tech-name">Tech</div>
+          <div className="technologies">
+            <div>React</div>
+            <div>Node js</div>
+            <div>Exress</div>
+            <div>MongoDB</div>
+            <div>JWT Authentication</div>
+            <div>CSS</div>
+          </div>
+        </div>
+        <div className="btn-pro">
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
     </div>
   )
 }
