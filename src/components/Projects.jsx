750a0b9e98b2de765c957842aa2b4e0db2ae70d8
ahmed <ahmedlearn01@gmail.com>
2022-03-23 06:37:06 -0700

    work

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      <div className="pro-wrap">
+        <img src="" alt="" />
+        <p></p>
+        <div className="tech">
+          <div className="tech-name">Tech</div>
+          <div className="technoligies">
+            <div>React</div>
+            <div>React</div>
+            <div>React</div>
+            <div>React</div>
+          </div>
+        </div>
+        <div className="btn-pro">
+          <button>Code</button>
+          <button>Demo</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
