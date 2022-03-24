d645d5afc54810a1a7acf62950fa83feb4109034
ahmed <ahmedlearn01@gmail.com>
2022-03-24 14:31:54 -0700

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,10 +6,11 @@ const Projects = () => {
       <div className="pro-wrap">
         <img src={pro1} alt="pro" />
         <h3>Somblog</h3>
-        <p>Complete Blog Website with Admin management & Moderators and User Login With Authentication</p>
+        <p>Complete Blog Website with Login and Registration user Authentication & Admin management & Moderators</p>
         <div className="tech">
           <div className="tech-name">Tech</div>
           <div className="technologies">
+            <div>Bcrypt js</div>
             <div>React</div>
             <div>Node js</div>
             <div>Exress</div>
@@ -26,16 +27,16 @@ const Projects = () => {
 
       <div className="pro-wrap">
         <img src={pro1} alt="pro" />
-        <h3>Somblog</h3>
-        <p>Complete Blog Website with Admin management & Moderators and User Login With Authentication</p>
+        <h3>Chat App</h3>
+        <p>Realtime Chat App With Socketio. chat rooms with colored message sents & multible user.</p>
         <div className="tech">
           <div className="tech-name">Tech</div>
           <div className="technologies">
+            <div>Socket-io</div>
             <div>React</div>
             <div>Node js</div>
             <div>Exress</div>
             <div>MongoDB</div>
-            <div>JWT Authentication</div>
             <div>CSS</div>
           </div>
         </div>
@@ -47,11 +48,13 @@ const Projects = () => {
 
       <div className="pro-wrap">
         <img src={pro1} alt="pro" />
-        <h3>Somblog</h3>
-        <p>Complete Blog Website with Admin management & Moderators and User Login With Authentication</p>
+        <h3>Mern Stack App</h3>
+        <p>Complete Mern stack app with Redux, Goal Setting App, sign in and register, goal setting and deleteing, jwt Authentication </p>
         <div className="tech">
           <div className="tech-name">Tech</div>
           <div className="technologies">
+            <div>Redux Toolkit</div>
+            <div>Bcrypt js</div>
             <div>React</div>
             <div>Node js</div>
             <div>Exress</div>
