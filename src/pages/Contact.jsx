622d3c085ea9b1389acfaf966871d0c890a33564
ahmed <ahmedlearn01@gmail.com>
2022-03-27 12:04:09 -0700

    some changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import ContactUs from "../components/ContactUs"
 
 const Contact = () => {
   return (
-    <div className="contact all">
+    <div className="contact all" id="contact">
       <h1>Contact Us</h1>
         <ContactUs />
     </div>
