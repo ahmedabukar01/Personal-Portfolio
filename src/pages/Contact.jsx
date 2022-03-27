7d2561fc86bb9b363b1a59d0ad63b919fee7f757
ahmed <ahmedlearn01@gmail.com>
2022-03-27 07:04:45 -0700

    commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,7 @@ import ContactUs from "../components/ContactUs"
 const Contact = () => {
   return (
     <div className="contact all">
+      <h1>Contact Us</h1>
         <ContactUs />
     </div>
   )
