6872a2bf681b951d95f6fea12b9fc3d2d3a0c873
ahmed <ahmedlearn01@gmail.com>
2022-05-12 06:48:53 -0700

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,13 @@
 import pro1 from '../images/work.jpg';
 import normed from '../images/phone2.PNG';
 import chat from '../images/chat.png';
+import myPortfolio from '../images/portfolio.PNG';
 
 const Projects = () => {
   return (
     <div className='projects'>
-      <div className="pro-wrap">
+      
+      {/* <div className="pro-wrap">
         <img src={pro1} alt="pro" />
         <h3>Somblog</h3>
         <p>Complete Blog Website with Login and Registration user Authentication & Admin management & Moderators</p>
@@ -23,7 +25,46 @@ const Projects = () => {
         </div>
         <div className="btn-pro">
           <a href="https://github.com/ahmedabukar01/somBlog">Code</a>
-          {/* <button className='not-available'>Demo</button> */}
+          <button className='not-available'>Demo</button>
+        </div>
+      </div> */}
+
+      <div className="pro-wrap">
+        <img src={normed} alt="pro" />
+        <h3>Nomed Technolgy</h3>
+        <p>Creative Web design and Development Agency in Mogadisho Somalia</p>
+        <div className="tech">
+          <div className="tech-name">Tech</div>
+          <div className="technologies">
+            <div>CSS</div>
+            <div>Flexible Box</div>
+            <div>React js</div>
+            <div>Netlify</div>
+          </div>
+        </div>
+        <div className="btn-pro">
+          <a href='https://github.com/ahmedabukar01/Normed'>Code</a>
+          <a href="https://normedtechnology.me/">Demo</a>
+        </div>
+      </div>
+
+      <div className="pro-wrap">
+        <img src={myPortfolio} alt="pro" />
+        <h3>Portfolio Website</h3>
+        <p>Unique and Creative Portfolio Website With React & Sass from scratch</p>
+        <div className="tech">
+          <div className="tech-name">Tech</div>
+          <div className="technologies">
+            <div>Particles js</div>
+            <div>Sass</div>
+            <div>React js</div>
+            <div>Netlify</div>
+            <div>UI & UX Design</div>
+          </div>
+        </div>
+        <div className="btn-pro">
+          <a href='https://github.com/ahmedabukar01/Personal-Portfolio'>Code</a>
+          <a href="https://ahmedabukar.me/">Demo</a>
         </div>
       </div>
 
@@ -71,24 +112,6 @@ const Projects = () => {
         </div>
       </div>
 
-      <div className="pro-wrap">
-        <img src={normed} alt="pro" />
-        <h3>Nomed Technolgy</h3>
-        <p>Creative Web design and Development Agency in Mogadisho Somalia</p>
-        <div className="tech">
-          <div className="tech-name">Tech</div>
-          <div className="technologies">
-            <div>CSS</div>
-            <div>Flexible Box</div>
-            <div>React js</div>
-            <div>Netlify</div>
-          </div>
-        </div>
-        <div className="btn-pro">
-          <a href='https://github.com/ahmedabukar01/Normed'>Code</a>
-          <a href="https://normedtechnology.me/">Demo</a>
-        </div>
-      </div>
     </div>
   )
 }
